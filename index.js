bae102727076e65b20ffcc427817f28cb3495c17
sashabeton <aleksa.yagich@gmail.com>
2021-01-27 11:58:04 +0200

    Change limit of expectation request

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const equal = require('deep-equal');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 
 let expectations = [];
 let errors = [];
